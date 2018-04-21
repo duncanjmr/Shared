7af2cc0e810ae47d774e584404078eb981dca1b3
Duncan <duncanjmr@gmail.com>
2018-04-21 15:31:14 -0700

    modified:   telecon.py 	new file:   tools.py

--- telecon.py
+++ telecon.py
@@ -1,52 +1,64 @@
-import urllib.request as urllib
+import requests
+import time
 import json
 
-class server:
+
+class telecon:
     def __init__(self):
         # Specify flask ip adress here (this one works if flask is run on your own computer)
-        self.url = 'http://localhost:5123'
-        
+        self.url = 'http://192.168.0.100:5123'
+
     def set_location(self,lon_lat,units):
         data = {'location': lon_lat,'units':units}
-        req = urllib.Request(self.url+'/set_location')
-        req.add_header('Content-Type', 'application/json')
-        response = urllib.urlopen(req, json.dumps(data))
+        resp = requests.post(self.url+'/set_location',data=json.dumps(data))
+        response = resp.content
+        return response
         
     def set_targ(self,az_alt):
         # Set the target position in the arduino (notice dictionary protocol)
         data = {'targ': az_alt}
-        req = urllib.Request(self.url+'/set_targ')
-        req.add_header('Content-Type', 'application/json')
-        response = urllib.urlopen(req, json.dumps(data))
+        resp = requests.post(self.url+'/set_targ',data=json.dumps(data))
+        return resp.content
         
     def get_pos(self):
         # Get the current position of the arduino (again, protocols)
-        a = urllib.urlopen(self.url+'/get_pos')
-        dic = json.loads(a.read())
+        resp = requests.get(self.url+'/get_pos')
+        dic = json.loads(resp.content)
         az_alt = dic['pos']
-        #az_alt_targ = dic['targ']
-        return az_alt#,az_alt_targ
+        return az_alt
     
     def setup(self):
+        
+        t = time.localtime()
+        string = ''
+        for i in t[:6]:
+            bit = str(i)
+            if len(bit)==1:
+                bit = '0'+bit
+            string += bit + '-'
+        string += '000'
+        # Currently nonfuntional
+        data = {'time': string}
+        
         # Currently nonfuntionalipython
-        a = urllib.urlopen(self.url+'/setup')
-        return json.loads(a.read())
-
+        a = requests.post(self.url+'/setup',data=json.dumps(data))
+        return json.loads(a.content)["calib_coors"]
+    
     def move(self,az_alt):
         # move a small incriment
         data = {'increment': az_alt}
-        req = urllib.Request(self.url+'/move')
-        req.add_header('Content-Type', 'application/json')
-        response = urllib.urlopen(req, json.dumps(data))
+        req = requests.post(self.url+'/move',data=json.dumps(data))
+        response = req.content
+        return response
         
     def set_calib(self,n=None):
         if n==None:
-            urllib.urlopen(self.url+'/set_calib')
+            requests.post(self.url+'/set_calib')
         else:
             data = {"calib_num": n }
-            req = urllib.Request(self.url+'/set_calib')
-            req.add_header('Content-Type', 'application/json')
-            response = urllib.urlopen(req, json.dumps(data))
+            req = requests.post(self.url+'/set_calib',data=json.dumps(data))
+            response = req.content
+            
     def calibrate(self):
-        urllib.urlopen(self.url+'/calibrate')
+        requests.get(self.url+'/calibrate')
         
---
+++ tools.py
@@ -0,0 +1,5 @@
+
+
+class tools():
+    def __init__(self):
+        self.picdir = '/storage/sdcard0/Pictures/'
